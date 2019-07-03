f4da34992ebe3bf972de9ff3773561f76ec8a31c
maeffju <43793922+maeffju@users.noreply.github.com>
2019-07-03 13:01:15 +0200

    initial commit

---
+++ KimControlInputRead/KimControlInputRead.ino
@@ -0,0 +1,57 @@
+
+#define FADER_OFFSET 69   // analog offset when fader is in zero-position
+
+char fader1 = A8;
+char fader2 = A11;
+char fader3 = A10;
+char fader4 = A13;
+char fader5 = A15;
+char fader6 = A9;
+char joystickX = A14;
+char joystickY = A12;
+char joystickButton = 32;
+char button1pin = 31;
+char button2pin = 33;
+char button3pin = 34;
+
+
+int fader1A = 0;
+int fader1APrev = 0;
+int fader2A = 0;
+int fader2APrev = 0;
+int fader3A = 0;
+int fader3APrev = 0;
+int joystickXA = 0;
+int joystickXAPrev = 0;
+int button1 = 0;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(fader1, INPUT);
+  pinMode(fader2, INPUT);
+  pinMode(fader3, INPUT);
+  pinMode(fader4, INPUT);
+  pinMode(fader5, INPUT);
+  pinMode(fader6, INPUT);
+  pinMode(joystickX, INPUT);
+  pinMode(joystickY, INPUT);
+  pinMode(joystickButton, INPUT_PULLUP);    // Pullup required. When pressed, buttons pull signal to ground.
+  pinMode(button1pin, INPUT_PULLUP);
+  pinMode(button2pin, INPUT_PULLUP);
+  pinMode(button3pin, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //joystickXA = analogRead(joystickX);
+  //fader1A = analogRead(fader1);
+  button1 = !digitalRead(button1pin);
+  Serial.println(button1);
+  delay(200);
+}
+
+int readFader(char faderNo){
+  int ret = analogRead(faderNo);
+}
---
+++ serial_receive/serial_receive.ino
@@ -0,0 +1,63 @@
+const char psuPin = 12;
+const char loadPin = 8;
+const String IDN = "Arduino Load Switch rev 1.0";
+
+String inputString = "";
+boolean commandReceived = false;
+
+void switchPSU(int output){
+  if(output == 0){
+    digitalWrite(psuPin, LOW);
+    Serial.println("OK PSU is off");
+  }
+  if(output == 1){
+    digitalWrite(psuPin, HIGH);
+    Serial.println("OK PSU is on");
+  }
+}
+
+void switchLOAD(int output){
+  if(output == 0){
+    digitalWrite(loadPin, LOW);
+    Serial.println("OK Load is off");
+  }
+  if(output == 1){
+    digitalWrite(loadPin, HIGH);
+    Serial.println("OK Load is on");
+  }
+  }
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(psuPin, OUTPUT);
+  pinMode(loadPin, OUTPUT);
+}
+
+void loop() {
+  if(commandReceived){     
+    if (inputString == "SWITCH:PSU:ON")
+      switchPSU(1);
+    if (inputString == "SWITCH:PSU:OFF")
+      switchPSU(0);
+    if (inputString == "SWITCH:LOAD:ON")
+      switchLOAD(1);
+    if (inputString == "SWITCH:LOAD:OFF")
+      switchLOAD(0);    
+    if (inputString == "*IDN?")
+      Serial.println(IDN);
+  }
+  commandReceived = false;
+}
+
+void serialEvent(){
+  commandReceived = false;
+  while(Serial.available()){
+    //long startReceive=millis(); 
+    inputString = Serial.readStringUntil('\n');
+    inputString.trim(); //remove whitespace from the end
+    inputString.toUpperCase(); // get upper-case version of the string
+    //Serial.println("read duration: " + String((millis() - startReceive), DEC));
+    commandReceived = true;
+    }
+  }
